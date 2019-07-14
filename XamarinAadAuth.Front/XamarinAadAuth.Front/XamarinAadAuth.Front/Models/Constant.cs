f0ebe5c490bc276e7d642b28c6e073ca1d63c766
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-07-15 01:53:36 +0900

    Add Constant

---
+++ XamarinAadAuth.Front/XamarinAadAuth.Front/XamarinAadAuth.Front/Models/Constant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinAadAuth.Front.Models
+{
+    public class Constant
+    {
+        public static string ClientId = "{Your Client ID}";
+        public static string BaseUrl = "https://{Your App Service}.azurewebsites.net";
+
+
+    }
+}
